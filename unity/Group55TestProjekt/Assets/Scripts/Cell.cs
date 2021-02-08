8ebb7cc4b76d9f52b63d85631e9d26ae14c312df
l0gren <emil.logren@gmail.com>
2021-02-08 18:51:07 +0100

    Add rudimentary description of cell internals and movement

---
+++ unity/Group55TestProjekt/Assets/Scripts/Cell.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    private bool run = true;
+    private float c = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SetConcentration(float c)
+    {
+        this.c = c;
+        DecideState();
+    }
+
+    public bool IsRun()
+    {
+        return this.run;
+    }
+
+    private void DecideState()
+    {
+        int rand = Random.Range(0,10);
+        if(rand <= 10*this.c)
+            this.run = true;
+        else 
+            this.run = false;
+    }
+}
---
+++ unity/Group55TestProjekt/Assets/Scripts/NewMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMovement : MonoBehaviour
+{
+
+    private Cell cell;
+    private bool run;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(UpdateState());
+    }
+
+    void Update()
+    {
+        if(run)
+            transform.Translate(Vector3.right * Time.deltaTime);
+        else
+            transform.Rotate(0.0f, 1.0f, 0.0f, Space.World);
+    }
+
+    private void Awake ()
+    {
+        cell = GetComponent<Cell>();
+    }
+
+    IEnumerator UpdateState()
+    {
+        while (true)
+        {
+            float c = Random.Range(0.0f,1.0f);
+            print(c);
+            yield return new WaitForSeconds(1);
+            cell.SetConcentration(c);
+            yield return new WaitForSeconds(0.5f);
+            this.run = cell.IsRun();
+        }
+    }
+}
