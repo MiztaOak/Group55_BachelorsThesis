26efa3278dfef63dacd193b7fd03485296899f31
MiztaOak <ek.johan98@gmail.com>
2021-02-09 16:32:57 +0100

    Implemented some of the code that is needed for the structure This consists of a basic model class implemnting a singleton patterns and a very basic factory for creating environments (this does not correctly implement the pattern). These classes will serve to connect the different parts of the program

---
+++ unity/Group55TestProjekt/Assets/Scripts/model/Model.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class that will manage the overarching data and operations that are needed by all of the program, sort of like the hub of the program
+public class Model
+{
+    private static Model instance;
+    public AbstractEnvironment environment { get; set; } //allows for the environment to be changed by the program if needed
+
+    private Model()
+    {
+        //add code as it is needed
+        environment = new BasicEnvironment(); //super base case just to prevent any scary null pointers
+    }
+
+    public static Model GetInstance()
+    {
+        if (instance == null)
+            instance = new Model();
+        return instance;
+    }
+
+    //Code bellow is a very dumb implementation but will be usefull before the ui is implemented
+
+    //creates a basic enironment
+    public void SetEnvironment(float d, float i_0, float x, float y)
+    {
+        environment = new BasicEnvironment(d, i_0, x, y);
+    }
+
+    //creates a time dependent environment
+    public void SetEnvironment(float d, float i_0, float x, float y, float maxTime, float k)
+    {
+        environment = new TimeDependentEnvironment(d, i_0, x, y, maxTime, k);
+    }
+}
