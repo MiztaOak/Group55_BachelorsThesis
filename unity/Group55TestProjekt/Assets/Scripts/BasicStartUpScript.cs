a2cc2388b790d93b6ac6a980af3eb0390b6e1825
MiztaOak <ek.johan98@gmail.com>
2021-02-15 17:38:37 +0100

    Changed the factory so that it would be better and renamed y to z

--- unity/Group55TestProjekt/Assets/Scripts/BasicStartUpScript.cs
+++ unity/Group55TestProjekt/Assets/Scripts/BasicStartUpScript.cs
@@ -8,7 +8,7 @@ public class BasicStartUpScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        EnvironmentFactory.CreateEnvironment(EnvironmentType.Basic, d, i_0, transform.position.x, transform.position.z, 0, 0);
+        EnvironmentFactory.CreateBasicEnvionment(d, i_0, transform.position.x, transform.position.z);
     }
 
     // Update is called once per frame
