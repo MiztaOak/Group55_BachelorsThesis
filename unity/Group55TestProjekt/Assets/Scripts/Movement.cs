753f97af24cacac014c1e07a21b3b0f55e65dcf3
MiztaOak <ek.johan98@gmail.com>
2021-02-21 18:48:41 +0100

    Added some very simple collision handeling for the walls

--- unity/Group55TestProjekt/Assets/Scripts/Movement.cs
+++ unity/Group55TestProjekt/Assets/Scripts/Movement.cs
@@ -12,6 +12,8 @@ public class Movement : MonoBehaviour
     public float rotSpeed;
     private Animator myAnimator;
 
+    private Rigidbody cellRigidBody;
+
     private int rotDir;
 
     // Start is called before the first frame update
@@ -19,6 +21,7 @@ public class Movement : MonoBehaviour
     {
         cell = new Cell();
         myAnimator = GetComponent<Animator>();
+        cellRigidBody = GetComponent<Rigidbody>(); //might have to be used later
         StartCoroutine(UpdateState());
     }
 
@@ -29,17 +32,18 @@ public class Movement : MonoBehaviour
             SceneManager.LoadScene(0);
         }
 
-        if (run)
+        /*if (run)
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime*-1);
         else
         {
             float angle = Random.Range(-180.0f, 180.0f);
             transform.Rotate(0.0f, rotDir*rotSpeed, 0.0f, Space.World);
-        }
+        }*/
     }
 
     IEnumerator UpdateState()
     {
+        Debug.Log("test");
         while (true)
         {
             Vector3 pos = transform.position;
@@ -61,4 +65,36 @@ public class Movement : MonoBehaviour
         }
     }
 
+    private void FixedUpdate() //update that has to be used for the rigid body if not the collisions wont work
+    {
+        if (run)
+
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime * -1);
+            //cellRigidBody.MovePosition(transform.position + Vector3.right * moveSpeed * Time.deltaTime * -1);
+        else
+        {
+            float angle = Random.Range(-180.0f, 180.0f);
+            transform.Rotate(0.0f, rotDir * rotSpeed, 0.0f, Space.World);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HandleCollision(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        HandleCollision(other);
+    }
+
+    private void HandleCollision(Collider other)
+    {
+        if (other.gameObject.name.Substring(0, 4) == "Wall")
+        {
+            Vector3 pos = transform.position;
+            transform.position = new Vector3(Mathf.Clamp(pos.x, -14f, 14f), pos.y, Mathf.Clamp(pos.z, -14f, 14f)); //dumb solution plz fix
+        }
+        
+    }
 }
