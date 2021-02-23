cc22f1c1bc766bdccb2511940ff3dc65f4281ba6
MiztaOak <ek.johan98@gmail.com>
2021-02-23 15:25:12 +0100

    Restructured the model and began working on the cell split

--- unity/Group55TestProjekt/Assets/Scripts/model/IntraCellular/Cell.cs
+++ unity/Group55TestProjekt/Assets/Scripts/model/IntraCellular/Cell.cs
@@ -10,11 +10,35 @@ public class Cell
 
     private Model model;
     private ICellRegulation regulator;
+    private IPointAdapter location, nextlocation;
+
+    private float v; //velocity
+    private float dT; //time step
+    private float angle;
+
+    public Cell(float x, float z, float v, float dT)
+    {
+        this.model = Model.GetInstance();
+        this.regulator = new HazardRegulation();
+
+        location = new Vector3Adapter(x,z);
+
+        this.v = v;
+        this.dT = dT;
+        angle = 0;
+    }
 
     public Cell()
     {
         this.model = Model.GetInstance();
         this.regulator = new HazardRegulation();
+
+        location = new Vector3Adapter(0, 0);
+    }
+
+    public void CalculateNextPosition()
+    {
+
     }
 
     public bool GetRunningState(float x, float z)
