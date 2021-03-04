afc3839ddfdc7d101be4f01daca5e4ff08addc5a
l0gren <emil.logren@gmail.com>
2021-03-04 22:56:44 +0100

    Added stiff ODE solver and tinkered with environment-scales. Not balanced, need halp

--- unity/Group55TestProjekt/Assets/Scripts/model/IntraCellular/ODERegulation.cs
+++ unity/Group55TestProjekt/Assets/Scripts/model/IntraCellular/ODERegulation.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.OdeSolvers;
@@ -42,14 +43,18 @@ public class ODERegulation : ICellRegulation
     private static double Bp0=q*Bt;
     private static double m0=5;
 
-    //Used to calculate tumble bias, arbitrary? Calculate somehow?
-    private static double YStarved = 4.083;
-
     //Keep track of calculated values
     private double Ap;
     private double Yp;
     private double Bp;
     private double m;
+    
+    private double Ap2;
+    private double Yp2;
+    private double Bp2;
+    private double m2 = 5;
+    private double S;
+    private double U;
     
 
     private static double L; //Ligand concentration
@@ -58,32 +63,84 @@ public class ODERegulation : ICellRegulation
     private static VectorBuilder<double> V = Vector<double>.Build;
     private Vector<double> V0 = V.DenseOfArray(new double[] {Ap0,Yp0,Bp0,m0});
 
+    private static double kbar1 = 48.571;
+    private static double kbar2 = 1382.714;
+    private static double kbar3 = 6;
+    private static double kbar4 = 8.686;
+    private static double kbar5 = 1;
+    private static double kbar6 = 0.121;
+    private static double alpha1 = 0.814;
+    private static double alpha2 = 28.214;
+    private static double gammaR = 0.00857;
+    private static double gammaB = 0.352;
+
     private static Vector<double> ODE( double t, Vector<double> y)
     {
         Vector<double> dy = V.Dense(4);
-        dy[0] = (1/(1+Math.Exp(N*(1-y[3]/2+Math.Log((1+L/Kaoff)/(1+L/Kaon))))))*k1*(At-y[0])-k2*y[0]*(Yt-y[1])-k3*y[0]*(Bt-y[2]);
-        dy[1] = k2*y[0]*(Yt-y[1])-k4*y[1]*Zt-k6*y[1];
-        dy[2] = k3*y[0]*(Bt-y[2])-k5*y[2];
+        dy[0] = (1/(1+Math.Exp(N*(1-y[3]/2+Math.Log((1+L/Kaoff)/(1+L/Kaon))))))*k1*(At-y[0])-k2*y[0]*(Yt-y[1])-k3*y[0]*(Bt-y[2]); //a
+        dy[1] = k2*y[0]*(Yt-y[1])-k4*y[1]*Zt-k6*y[1]; //y
+        dy[2] = k3*y[0]*(Bt-y[2])-k5*y[2]; //b
         dy[3] = (gR*Rt*(1-(1/(1+Math.Exp(N*(1-y[3]/2+Math.Log((1+L/Kaoff)/(1+L/Kaon)))))))-
-                gB*Math.Pow(y[2],2)*(1/(1+Math.Exp(N*(1-y[3]/2+Math.Log((1+L/Kaoff)/(1+L/Kaon)))))));
+                gB*Math.Pow(y[2],2)*(1/(1+Math.Exp(N*(1-y[3]/2+Math.Log((1+L/Kaoff)/(1+L/Kaon))))))); //m
         return dy;
+    }
+
+    private double h(double y)
+    {
+        return 0.02+0.5*y;
+    }
+
+    private void SolveStiff(float c)
+    {
+        // L = (double) 0.001 + 9.99 * c;
+        L = (double) 10* c;
+        var sol = Ode.GearBDF(
+            0,
+            new Vector(Ap2, Yp2, Bp2, m2, S),
+            (t,x) => new Vector(
+                (1/(1+Math.Exp(N*(1-x[3]/2+Math.Log((1+L/Kaoff)/(1+L/Kaon))))))*kbar1*(1-x[0])-kbar2*x[0]*(1-x[1])-k3*x[0]*(1-x[2]), //a
+                alpha1*kbar2*x[0]*(1-x[1])-(kbar4+kbar6)*x[1], //y
+                alpha2*kbar3*x[0]*(1-x[2])-kbar5*x[2], //b
+                (gammaR*(1-(1/(1+Math.Exp(N*(1-x[3]/2+Math.Log((1+L/Kaoff)/(1+L/Kaon)))))))-
+                 gammaB*Math.Pow(x[2],2)*(1/(1+Math.Exp(N*(1-x[3]/2+Math.Log((1+L/Kaoff)/(1+L/Kaon))))))), //m
+                h(x[1])*(1-x[4])  
+            )
+        );
+        var points = sol.SolveFromTo(0, 1).ToArray();
+        var result = points[points.Length-1];
+        Ap2 = result.X[0];
+        Yp2 = result.X[1];
+        Bp2 = result.X[2];
+        m2  = result.X[3];
+        S   = result.X[4];
+        Debug.Log("Conc.: " + L);
+        // Debug.Log("Ap: " + Ap2);
+        Debug.Log("Yp: " + Yp2);
+        Debug.Log("S: " + S);
+        Debug.Log("U: " + U);
+        // Debug.Log("Bp: " + Bp2);
+        // Debug.Log("m: " + m2);
     }
 
     public bool DecideState(float c)
     {
-        var odeFunc = new Func<double, Vector<double>, Vector<double>>( ODE );
-        L = 0.09 + ( (double) c * 0.01 ); //Only values between 0.09 and 0.1 have effect 
-        Vector<double> V1 = RungeKutta.FourthOrder(V0, 0, 0.1, 1000, odeFunc)[900];
-        Ap = V1[0];
-        Yp = V1[1];
-        Bp = V1[2];
-        m = V1[3];
-        V0 = V.DenseOfArray(new double[] {Ap,Yp,Bp,m});
-        // double bias = 1 / (1 + (3/7)*Math.Pow((Yp/YStarved), 5.5)); //Taken from article, always 1??
-        double bias = Yp / Yt;
-        double r = rand.NextDouble();
-        if( bias > r )
+        SolveStiff(c);
+        // var odeFunc = new Func<double, Vector<double>, Vector<double>>( ODE );
+        // L = 0.09 + ( (double) c * 0.01 ); //Only values between 0.09 and 0.1 have effect 
+        // Vector<double> V1 = RungeKutta.FourthOrder(V0, 0, 0.1, 1000, odeFunc)[900];
+        // Ap = V1[0];
+        // Yp = V1[1];
+        // Bp = V1[2];
+        // m = V1[3];
+        // V0 = V.DenseOfArray(new double[] {Ap,Yp,Bp,m});
+        // double bias = Yp / Yt;
+        // double r = rand.NextDouble();
+        if( S > U )
+        {
+            S = 0.0f;
+            U = UnityEngine.Random.Range(0.0f,1.0f);
             return false; //Tumbling
+        }
         else
             return true; //Running 
     }
