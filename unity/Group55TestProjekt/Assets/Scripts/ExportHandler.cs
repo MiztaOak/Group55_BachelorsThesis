c25285844e409c14193f08c45f1654f560737942
MIDO-96 <44136378+MIDO-96@users.noreply.github.com>
2021-03-25 20:56:41 +0100

    f**k. I should remember to commit more frequently
    data exporting

---
+++ unity/Group55TestProjekt/Assets/Scripts/ExportHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public static class ExportHandler
+{
+    public static readonly string dirPath = Application.dataPath + "/Exports/";
+
+    public static void init()
+    {
+        if (!Directory.Exists(dirPath))
+        {
+            Directory.CreateDirectory(dirPath);
+        }
+            
+    }
+
+    public static void exportData(List<Model.DataToExport> dataToExport)
+    {
+        using (StreamWriter file = File.CreateText(dirPath+ "exported_data.json"))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            //serialize object directly into file stream
+            serializer.Serialize(file, dataToExport);
+        }
+    }
+}
